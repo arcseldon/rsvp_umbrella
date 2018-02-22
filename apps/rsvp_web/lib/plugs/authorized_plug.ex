66f3656c00e02bba1b43f479f1416180457e7af9
Roberto Estrada <restrada@paridin.com>
2018-02-22 17:12:55 -0600

    relax login

--- apps/rsvp_web/lib/plugs/authorized_plug.ex
+++ apps/rsvp_web/lib/plugs/authorized_plug.ex
@@ -7,20 +7,19 @@ defmodule RsvpWeb.AuthorizedPlug do
         options
     end
 
-    def call(conn, name) do
+    def call(conn, _name) do
         user_name = conn.cookies["user_name"]
-        authorize_user(conn, user_name, name)
+        authorize_user(conn, user_name)
     end
 
-    defp authorize_user(conn, nil, _) do
+    defp authorize_user(conn, nil) do
         conn
         |> redirect(to: "/login")
         |> halt # prevents call more plugs
     end
 
-    defp authorize_user(conn, user_name, name) when user_name === name do
+    defp authorize_user(conn, user_name) do
         conn
     end
 
-    defp authorize_user(conn, _, _), do: authorize_user(conn, nil, nil)
 end
